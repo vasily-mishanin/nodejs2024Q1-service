e1c34f0725bece9f3b9a4f4d7d519ba69b426a28
vasily <vasilymishanin@gmail.com>
2024-03-05 10:43:46 +0600

    init: nest app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = process.env.PORT || 4001;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(port, () => console.log('Server is running on port ', port));
 }
 bootstrap();
