a906e589ec313d3a8929ccff9b70968fa41274f0
vasily <vasilymishanin@gmail.com>
2024-03-10 20:26:51 +0600

    configure swagger docs

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { FakeDatabase } from './utils/data/fakeDatabase';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as YAML from 'yamljs';
 
 export const db = new FakeDatabase();
 
@@ -11,6 +13,12 @@ const port = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const document = YAML.load('doc/api.yaml');
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port, () => console.log('Server is running on port ', port));
 }
+
 bootstrap();
